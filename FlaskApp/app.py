d6879a6c33b720b159587e6a2a6760e80ae44aa3
elizamaharani <elizamaharani@gmail.com>
2023-03-03 18:26:09 +0700

    detail update

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 df = pd.read_csv('D:\TA CODING\SISTEM REKOMENDASI - ELIZA\Recommender-System-for-Skincare-Products-by-Eliza\FlaskApp\data\coba.csv')
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return render_template('katalog.html')
+  df_html = df.values
+  return render_template('katalog.html', data=df_html)
 
 @app.route('/katalog', methods=['GET'])
 def katalog():
@@ -18,9 +19,15 @@ def katalog():
 def rekomendasi():
   return render_template('rekomendasi.html')
 
-@app.route('/detail')
+@app.route('/detail/')
 def detail():
-  return render_template('detail.html')
+  Id = request.args.get("id")
+  df_html = df.values
+  for i in range(0, len(df_html)):
+    num = i+1
+    if Id == str(num) :
+      the_data = df_html[i]
+      return render_template('detail.html', data=the_data)
 
 if __name__ == '__main__':
   app.run(debug=True)
