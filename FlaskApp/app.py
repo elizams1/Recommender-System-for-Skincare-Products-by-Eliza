2229c01595ddb26e8b6990e2d11b336d0cbeade8
elizamaharani <elizamaharani@gmail.com>
2023-06-24 10:53:37 +0700

    update code

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -25,15 +25,17 @@ nltk.download('stopwords')
 # pip install -U scikit-learn
 
 df_product = pd.read_csv(
-    'D:\TA CODING\SISTEM REKOMENDASI - ELIZA\Recommender-System-for-Skincare-Products-by-Eliza\FlaskApp\data\DataProduk3.csv')
+    'D:\TA CODING\SISTEM REKOMENDASI - ELIZA\Recommender-System-for-Skincare-Products-by-Eliza\FlaskApp\data\DataProdukVers5.csv')
 
 df_user = pd.read_csv(
-    'D:\TA CODING\SISTEM REKOMENDASI - ELIZA\Recommender-System-for-Skincare-Products-by-Eliza\FlaskApp\data\datasetPengguna.csv')
+    'D:\TA CODING\SISTEM REKOMENDASI - ELIZA\Recommender-System-for-Skincare-Products-by-Eliza\FlaskApp\data\DataPengguna4.csv')
 
-df_dataset = pd.read_csv(
-    'D:\TA CODING\SISTEM REKOMENDASI - ELIZA\Recommender-System-for-Skincare-Products-by-Eliza\FlaskApp\data\datasetProduct.csv')
+df_dataset = pd.read_excel(
+    'D:\TA CODING\SISTEM REKOMENDASI - ELIZA\Recommender-System-for-Skincare-Products-by-Eliza\FlaskApp\data\Dataset Digabungkan.xlsx')
 
 app = Flask(__name__)
+
+df_user["Kandungan"].fillna(" ", inplace=True)
 
 # mengambil value dari dataframe df_product
 df_html = df_product.values
@@ -41,8 +43,9 @@ df_html = df_product.values
 # mengambil value dari dataframe df_user
 df_user_value = df_user.values
 
-
 # mendapatkan 10 data per halaman
+
+
 def get_datas(offset=0, per_page=10):
     return df_html[offset: offset + per_page]
 
@@ -53,26 +56,12 @@ def get_search(data, offset=0, per_page=10):
     return data[offset: offset + per_page]
 
 
-def get_recommendations_product2(user_input):
-
-    # Stopword Bahasa Indonesia
-    indo_stopword = set(stopwords.words('indonesian'))
-
-    # Stopword Bahasa Inggris
-    eng_stopword = set(stopwords.words('english'))
-
-    # Data Stopword
-    data_stopword = str(indo_stopword) + " " + str(eng_stopword)
-
-    # TFIDF
-    vectorizer = TfidfVectorizer(
-        analyzer='word', stop_words=list(data_stopword))
-    tfidf_dataset = vectorizer.fit_transform(df_dataset['data'])
-
-    # CASE FOLDING
+# TEXT PREPROCESSING INPUTAN
+def text_pre_input_two(user_input):
+  # CASE FOLDING
     user_input = user_input.lower()
 
-    # TOKENIZING
+  # TOKENIZING
     # Menghapus Angka
     user_input = re.sub(r"\d+", "", user_input)
     # Menghapus Tanda Baca
@@ -86,17 +75,24 @@ def get_recommendations_product2(user_input):
     user_input = re.sub(r"\n", "", user_input)
     # Menghapus berlebihan spasi
     user_input = re.sub('\s+', ' ', user_input)
-
     # dilakukan proses tokenization pada user_input
     tokens_query = [str(tok) for tok in nltk.word_tokenize(user_input)]
 
-    # FILTERING
+  # FILTERING
+    # Stopword Bahasa Indonesia
+    indo_stopword = set(stopwords.words('indonesian'))
+
+    # Stopword Bahasa Inggris
+    eng_stopword = set(stopwords.words('english'))
+
+    # Data Stopword
+    data_stopword = str(indo_stopword) + " " + str(eng_stopword)
+
     # Data tanpa stopword
     tokens_query = [word for word in tokens_query if not word in data_stopword]
 
-    # STEMMING
+  # STEMMING
     the_query = []
-
     # Membuat stemmer
     Factory = StemmerFactory()
     Stemmer = Factory.create_stemmer()
@@ -104,42 +100,33 @@ def get_recommendations_product2(user_input):
         tokens_query[i] = Stemmer.stem(tokens_query[i])
         the_query.append(tokens_query[i])
 
-    # Cetak query yang telah dilakukan stemming
-    print(the_query)
-    # Melakukan perhitungan TFIDF terhadap inputan pengguna kepada keseluruhan data kata
-    embed_query = vectorizer.transform(the_query)
+    join_query = ' '.join(tokens_query)
+    return (join_query)
 
-    # besar matriks TFIDF inputan pengguna
-    print(embed_query.shape)
 
+def get_recommendations_product_two(tfidf_dataset):
     # Mencari tingkat kemiripan antara user_input dengan dataset
-    cos_sim = cosine_similarity(embed_query, tfidf_dataset)
-
-    # Membuat dataframe untuk hasil similaritas
+    cos_sim = cosine_similarity(tfidf_dataset, tfidf_dataset)
+    # print(cos_sim)
     df_cosim = pd.DataFrame(data=cos_sim)
-    # Menampilkan dataframe
     # display(df_cosim)
 
-    # Mencari rata rata dari nilai kemiripan antara inputan pengguna dengan data produk
-    cos_sim_a = np.mean(cos_sim, axis=0)
-    # Membuat dataframe hasil rata rata
-    df_cosim_a = pd.DataFrame(data=cos_sim_a, columns=[
-                              "cosine similarity score"])
-    # Menampilkan hasil nilai kemiripan
-    # display(df_cosim_a)
+    # Mengambil nilai cosine similarity pada baris yang berisi inputan pengguna
+    val_cos_sim = pd.DataFrame(
+        data=cos_sim[-1, :], columns=['Cosine Similarity Score'])
+    # display(val_cos_sim)
 
     # Mencari 5 teratas nilai kemiripan tertinggi
-    top5 = df_cosim_a.sort_values(
-        "cosine similarity score", ascending=False).head(5)
-    # Menampilkan 5 nilai kemiripan teratas
+    top5 = val_cos_sim.sort_values(
+        "Cosine Similarity Score", ascending=False)[1:6]
     # display(top5)
+
     # Mencari index dari 5 nilai kemiripan teratas
     index = list(top5.index.values)
 
-    # Mencetak index
-    print(index)
+    return index, top5
 
-    return index
+# HALAMAN UTAMA
 
 
 @app.route('/')
@@ -158,6 +145,8 @@ def main():
                            products=products,
                            pagination=pagination,  page=page,
                            per_page=per_page)
+
+# HALAMAN KATALOG
 
 
 @app.route('/katalog', methods=['POST', 'GET'])
@@ -207,44 +196,95 @@ def rekomendasi():
 
         data_input_user = ""
         iP = ""
-        tP = type_product 
-        tS = type_skin 
+        tP = type_product
+        tS = type_skin
         pS = problem_skin
 
         for i in range(0, len(df_user_value)):
-            if df_user_value[i][0] == type_product and df_user_value[i][1] == type_skin and df_user_value[i][2] == problem_skin:
-                print(df_user_value[i][0] + " " + df_user_value[i][1] +
-                      " " + df_user_value[i][2] + " " + df_user_value[i][3])
-                data_input_user = df_user_value[i][0] + " " + df_user_value[i][1] + \
-                    " " + df_user_value[i][2] + " " + df_user_value[i][3]
-                if df_user_value[i][1] == 'Normal' and df_user_value[i][2] == 'Normal':
+            if df_user_value[i][1] == type_product and df_user_value[i][2] == type_skin and df_user_value[i][3] == problem_skin:
+                print(df_user_value[i][1] + " " + df_user_value[i][2] +
+                      " " + df_user_value[i][3] + " " + df_user_value[i][4])
+                data_input_user = df_user_value[i][1] + " " + df_user_value[i][2] + \
+                    " " + df_user_value[i][3] + " " + df_user_value[i][4]
+                if df_user_value[i][2] == 'Normal' and df_user_value[i][3] == 'Normal':
                     iP = " yang cocok untuk kulit yang normal "
                 else:
-                    iP = " " + df_user_value[i][3]
+                    iP = " " + df_user_value[i][4]
 
         if data_input_user == "":
-            the_output = "Isi form diatas untuk mendapatkan rekomendasi produk skincare"
+            the_output = "Isian Belum Lengkap! Isi form diatas untuk mendapatkan rekomendasi produk skincare"
             return render_template('rekomendasi.html', data2=the_output)
 
         else:
-            the_output2 = get_recommendations_product2(data_input_user)
+            the_output2 = text_pre_input_two(data_input_user)
+            print(the_output2)
+            add_data = pd.DataFrame({'data': the_output2}, index=[-1])
+            the_data = df_html
+
+            # percabangan agar produk yang direkomendasikan sesuai dengan kategori
+            if (data_input_user.find('Facial Wash') != -1):
+                print('Facial Wash')
+                th_idx = pd.concat(
+                    [df_dataset[0:20], add_data]).reset_index(drop=True)
+                the_product = the_data[0:20]
+            elif (data_input_user.find('Face Toner') != -1):
+                print('Face Toner')
+                th_idx = pd.concat(
+                    [df_dataset[20:40], add_data]).reset_index(drop=True)
+                the_product = the_data[20:40]
+            elif (data_input_user.find('Face Serum') != -1):
+                print('Face Serum')
+                th_idx = pd.concat(
+                    [df_dataset[40:60], add_data]).reset_index(drop=True)
+                the_product = the_data[40:60]
+            elif (data_input_user.find('Moisturizer') != -1):
+                print('Moisturizer')
+                th_idx = pd.concat(
+                    [df_dataset[60:80], add_data]).reset_index(drop=True)
+                the_product = the_data[60:80]
+            else:
+                print('Sunscreen')
+                th_idx = pd.concat(
+                    [df_dataset[80:100], add_data]).reset_index(drop=True)
+                the_product = the_data[80:100]
+
+            # Stopword Bahasa Indonesia
+            indo_stopword = set(stopwords.words('indonesian'))
+
+            # Stopword Bahasa Inggris
+            eng_stopword = set(stopwords.words('english'))
+
+            # Data Stopword
+            data_stopword = str(indo_stopword) + " " + str(eng_stopword)
+
+            # TFIDF vectorizer
+            vectorizer = TfidfVectorizer(
+                analyzer='word', stop_words=list(data_stopword))
+            tfidf_dataset1 = vectorizer.fit_transform(th_idx['data'])
+            index, top5 = get_recommendations_product_two(tfidf_dataset1)
+            print(index)
 
             data_res = []
-            for value in df_html:
-                for i in the_output2:
+            for value in the_product:
+                for i in index:
                     if value[0] == i+1:
                         data_res.append(value)
 
             the_output = data_input_user + " " + str(the_output2)
+            print(the_output)
+
             data2 = "Hallo Besti, Min Glowink akan rekomendasiin " + \
                 str(tP) + " untuk jenis kulit " + str(tS) + \
-                " yang membantu kamu merawat permasalahan kulit "+ pS +  " karena produk ini memiliki kandungan " + iP
+                " yang membantu kamu merawat permasalahan kulit " + \
+                pS + " karena produk ini memiliki kandungan " + iP
             return render_template('rekomendasi.html', data=data_res, data2=data2)
 
     # REKOMENDASI START
     else:
         the_output = "Isi form diatas untuk mendapatkan rekomendasi produk skincare"
         return render_template('rekomendasi.html', data2=the_output)
+
+# DETAIL PAGE
 
 
 @app.route('/detail/')
