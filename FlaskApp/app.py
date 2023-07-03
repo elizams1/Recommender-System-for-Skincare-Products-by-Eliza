9ef7e8220a41e664669383a726e989b32058a905
elizamaharani <elizamaharani@gmail.com>
2023-07-03 07:46:58 +0700

    d

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -296,6 +296,6 @@ def detail():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
 
 #new
--- FlaskApp/coba.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, static_url_path='/FlaskApp/static', static_folder="static")
-
-
-@app.route("/")
-def hello_world():
-    return "Hello World! This is me"
